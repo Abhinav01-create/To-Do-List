2419eaaa7c42eff652bc2c1eb61c24f3223668ba
abhinavsinghchauhan <your.email@example.com>
2025-12-28 14:26:17 +0530

    Add index file to the To-Do list.

---
+++ list.js
@@ -0,0 +1,38 @@
+const searchInput = document.getElementById("search-input");
+const bullet = document.getElementById("bullet");
+
+function addTask(){
+    if(searchInput.value === ''){
+        alert('Input a valid entry.');
+    }
+    else{
+        let li = document.createElement('li');
+        li.innerHTML=searchInput.value;
+        bullet.appendChild(li);
+        let span = document.createElement('span');
+        span.innerHTML="\u00d7";
+        li.appendChild(span);
+    }
+    searchInput.value='';
+    saveData();
+}
+
+bullet.addEventListener("click", function(e){
+    if(e.target.tagName === "LI"){
+        e.target.classList.toggle("checked");
+        saveData();
+    }
+    else if(e.target.tagName==="SPAN"){
+        e.target.parentElement.remove();
+        saveData();
+    }
+},false);
+
+function saveData(){
+    localStorage.setItem("data",bullet.innerHTML)
+}
+function showTask(){
+    bullet.innerHTML=localStorage.getItem("data");
+}
+showTask();
+
